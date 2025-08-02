8e8a595cc90c63c3592192c40b11b0490f9c0f3f
Kim Pham <km.phm2@gmail.com>
2025-08-03 01:49:35 +1000

    Add basic styling

---
+++ widget.js
@@ -0,0 +1,8 @@
+/*
+slot machine
+- 3 vertical reels containing symbols - emojis
+- spin button: when clicked, spin all reels. after certain time, the reels will stop one by one
+- the left reel will stop, then middle, then right --> after small delay between each reel
+- if the symbols match, player wins
+- if symbols dont match, player can spin again (nothing happens)
+*/
